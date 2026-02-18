4aceb727b932a7b1054b77b0fad62740a9f32343
ZerO Execption <swk.pt16@gmail.com>
2026-02-18 20:52:20 +0700

    init

--- server.js
+++ server.js
@@ -13,6 +13,5 @@ app.use(`/api/v1/hospitals`,hospitals);
 
 
 
-
 const PORT=process.env.PORT || 5000;
 app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV,' mode on port ', PORT ));
