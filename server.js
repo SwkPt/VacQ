a2a39d6cb8444a64128070625b5985f6d0b7657b
ZerO Execption <swk.pt16@gmail.com>
2026-02-19 02:18:32 +0700

    connect to MongoDB

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
+const { setServers } = require("node:dns/promises");
+setServers(["1.1.1.1", "8.8.8.8"]);
 const express = require('express');
 const dotenv = require('dotenv');
-
-const hospitals = require(`./routes/hospitals`);
+const connectDB = require('./config/db')
 
 dotenv.config({path:'./config/config.env'});
 
+connectDB();
+
+const hospitals = require(`./routes/hospitals`);
+
+
 const app=express();
 
-
+app.use(express.json())
 
 app.use(`/api/v1/hospitals`,hospitals);
 
 
-
 const PORT=process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV,' mode on port ', PORT ));
+const Server =  app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV,' mode on port ', PORT ));
+
+process.on('unhandledRejection', (err,promise) =>{
+    console.log(`Error: ${err.message}`);
+    Server.close(()=>process.exit(1));
+} )
