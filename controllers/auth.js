0b8d2c46e330a5e204cc618a2c88a56cf1555f32
ZerO Execption <swk.pt16@gmail.com>
2026-02-20 10:47:21 +0700

    Authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,9 +13,9 @@ exports.register = async (req, res, next) => {
             role
         });
 
-        const token = user.getSignedJwtToken();
-
-        res.status(200).json({success:true, token})
+        //const token = user.getSignedJwtToken();
+        //res.status(200).json({success:true, token})
+        sendTokenResponse(user,200,res);
     } catch (err) {
         res.status(400).json({success:false});
         console.log(err.stack);
@@ -46,7 +46,40 @@ exports.login = async (req, res, next) => {
     }
 
     // Create token
+    //const token=user.getSignedJwtToken();
+    //res.status(200).json({success:true, token});
+    sendTokenResponse(user,200,res);
+
+};
+
+// Get token from model, create cookie and send response
+const sendTokenResponse=(user, statusCode, res)=>{
+    // Create token
     const token=user.getSignedJwtToken();
 
-    res.status(200).json({success:true, token});
+    const options = {
+        expires:new Date(Date.now()+process.env.JWT_COOKIE_EXPIRE*24*60*60*1000),
+        httpOnly: true
+    };
+
+    if(process.env.NODE_ENV==='production'){
+        options.secure=true;
+    }
+
+    res.status(statusCode).cookie('token',token,options).json({
+        success: true,
+        token
+    })
+}
+
+//At the end of file
+//@desc      Get current Logged in user
+//@route     POST /api/v1/auth/me
+//@access    Private
+exports.getMe=async(req,res,next)=>{
+    const user=await User.findById(req.user.id);
+    res.status(200).json({
+        success:true,
+        data:user
+    });
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { setServers } = require("node:dns/promises");
 setServers(["1.1.1.1", "8.8.8.8"]);
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db')
 
 dotenv.config({path:'./config/config.env'});
@@ -13,7 +14,9 @@ const auth = require('./routes/auth');
 
 const app=express();
 
-app.use(express.json())
+app.use(express.json());
+
+app.use(cookieParser());
 
 app.use('/api/v1/hospitals',hospitals);
 app.use('/api/v1/auth',auth);
@@ -24,4 +27,4 @@ const Server =  app.listen(PORT, console.log('Server running in ', process.env.N
 process.on('unhandledRejection', (err,promise) =>{
     console.log(`Error: ${err.message}`);
     Server.close(()=>process.exit(1));
-} )
+} );
