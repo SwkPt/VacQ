1065319dc1e9565dbbb5eabe024e80500ba5ba54
ZerO Execption <swk.pt16@gmail.com>
2026-02-26 12:47:43 +0700

    API sec

--- controllers/auth.js
+++ controllers/auth.js
@@ -26,30 +26,35 @@ exports.register = async (req, res, next) => {
 // @route     POST /api/v1/auth/login
 // @access    Public
 exports.login = async (req, res, next) => {
-    const {email, password}=req.body;
+    try {
+        
+     
+        const {email, password}=req.body;
 
-    if(!email || !password){
-        return res.status(400).json({success:false, msg:'Please provide an email and password'});
+        if(!email || !password){
+            return res.status(400).json({success:false, msg:'Please provide an email and password'});
+        }
+
+        const user = await User.findOne({email}).select('+password');
+        
+        if(!user){
+            return res.status(400).json({success:false, msg:'Invalid credentials'});
+        }
+
+        // Check if password matches
+        const isMatch = await user.matchPassword(password);
+
+        if(!isMatch){
+            return res.status(401).json({success:false, msg:'Invalid credentials'});
+        }
+
+        // Create token
+        //const token=user.getSignedJwtToken();
+        //res.status(200).json({success:true, token});
+        sendTokenResponse(user,200,res);
+    }catch (error) {
+        return res.status(401).json({success:false, msg:'Cant convert email/password to string'});
     }
-
-    const user = await User.findOne({email}).select('+password');
-    
-    if(!user){
-        return res.status(400).json({success:false, msg:'Invalid credentials'});
-    }
-
-    // Check if password matches
-    const isMatch = await user.matchPassword(password);
-
-    if(!isMatch){
-        return res.status(401).json({success:false, msg:'Invalid credentials'});
-    }
-
-    // Create token
-    //const token=user.getSignedJwtToken();
-    //res.status(200).json({success:true, token});
-    sendTokenResponse(user,200,res);
-
 };
 
 // Get token from model, create cookie and send response
@@ -72,7 +77,7 @@ const sendTokenResponse=(user, statusCode, res)=>{
     })
 }
 
-//At the end of file
+
 //@desc      Get current Logged in user
 //@route     POST /api/v1/auth/me
 //@access    Private
@@ -81,5 +86,20 @@ exports.getMe=async(req,res,next)=>{
     res.status(200).json({
         success:true,
         data:user
+    });
+};
+
+//@desc      logout/clear cookie
+//@route     GET /api/v1/auth/logout
+//@access    Private
+exports.logout = async (req,res,next) => {
+    res.cookie('token','none',{
+        expires:new Date(Date.now()+10*1000),//logout in 10 secs
+        httpOnly:true
+    });
+    
+    res.status(200).json({
+        success:true,
+        data:{}
     });
 };
--- server.js
+++ server.js
@@ -4,6 +4,14 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser')
 const connectDB = require('./config/db')
+const mongoSanitize = require('@exortek/express-mongo-sanitize');
+const helmet = require('helmet');
+const {xss}=require('express-xss-sanitizer');
+const rateLimit=require('express-rate-limit');
+const hpp=require('hpp');
+const cors=require('cors');
+
+
 
 dotenv.config({path:'./config/config.env'});
 
@@ -13,11 +21,27 @@ const hospitals = require('./routes/hospitals');
 const appointments =require('./routes/appointments');
 const auth = require('./routes/auth');
 
+
+
+
 const app=express();
 app.set('query parser', 'extended');
 app.use(express.json());
-
+app.use(helmet());
+app.use(mongoSanitize());
 app.use(cookieParser());
+app.use(xss());
+
+const limiter=rateLimit({
+    windowMs:10*60*1000,//10 mins
+    max: 100
+});
+
+app.use(limiter);
+app.use(hpp());
+app.use(cors());
+
+
 
 app.use('/api/v1/hospitals',hospitals);
 app.use('/api/v1/auth',auth);
